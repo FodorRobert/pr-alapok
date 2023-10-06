00bb83936160892000815d43667f0bcc76c931af
FodorRobert <113088682+FodorRobert@users.noreply.github.com>
2023-10-06 13:02:36 +0200

    Add files via upload

---
+++ 3-4-ora/feladat_007.py
@@ -0,0 +1,10 @@
+gondolt_szám = 4
+tipp = input ('Gondoltam egy számra. Tippeld meg!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print('Ügyes vagy!')
+    print('Gratulálok!')
+else:
+    print('Hosszan gondolkoztál rajta?:)')
+    print('Nem érte meg.;)')    
+print('Pápá!')    
