2daeea6635f34b7fcaea56e65cbba196e7824cbd
FodorRobert <113088682+FodorRobert@users.noreply.github.com>
2023-12-08 13:00:39 +0100

    Add files via upload

---
+++ Feladat/feladat_027.py
@@ -0,0 +1,9 @@
+# feladat_027
+# Lista bejárása index-szel I.
+
+honapok = ['január', 'február','március', 'április', 'május', 'június']
+
+index = 0
+for honap in honapok:
+    print(index, honap)
+    index = index + 1
